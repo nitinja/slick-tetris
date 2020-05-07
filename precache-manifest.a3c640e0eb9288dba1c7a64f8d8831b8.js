5d6a4c4b49ae6d96cc7b5f33ef0cbaf62e1fffc9
Nitin Jadhav <nitinjadhav@gmail.com>
2020-05-07 22:14:22 +0530

    Updates

---
+++ precache-manifest.a3c640e0eb9288dba1c7a64f8d8831b8.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a9aa7a5199d1f3ac76fbb6538ecf3b9",
+    "url": "/slick-tetris/index.html"
+  },
+  {
+    "revision": "775610d79c569b4e4699",
+    "url": "/slick-tetris/static/css/main.1af0724f.chunk.css"
+  },
+  {
+    "revision": "7ac10094b71ecf2879f0",
+    "url": "/slick-tetris/static/js/2.aa593617.chunk.js"
+  },
+  {
+    "revision": "775610d79c569b4e4699",
+    "url": "/slick-tetris/static/js/main.2828aab5.chunk.js"
+  },
+  {
+    "revision": "cea06e847c43f02f070b",
+    "url": "/slick-tetris/static/js/runtime-main.62ad5ea3.js"
+  },
+  {
+    "revision": "ddc250127189a3b4ee4802e6c2493757",
+    "url": "/slick-tetris/static/media/github.ddc25012.svg"
+  },
+  {
+    "revision": "ca1bcf064ef673b7eccadd2676eecb26",
+    "url": "/slick-tetris/static/media/noun_pause_170042.ca1bcf06.svg"
+  },
+  {
+    "revision": "614647a23ecce9e1813f371e1529c691",
+    "url": "/slick-tetris/static/media/noun_play_170039.614647a2.svg"
+  },
+  {
+    "revision": "8311a56ada6b8699d714b86a79584161",
+    "url": "/slick-tetris/static/media/noun_restart_170019.8311a56a.svg"
+  }
+]);
